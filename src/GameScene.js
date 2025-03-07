1430d38d54ca9a9af44d1907722f4498d440643d
Rafael Santana Rodrigues <138138154+RafaelSR44@users.noreply.github.com>
2025-03-07 12:01:49 -0300

    mudanca index

--- src/GameScene.js
+++ src/GameScene.js
@@ -7,7 +7,7 @@ class GameScene extends Phaser.Scene {
     this.jogadorAtirador = data.jogadorAtirador;
     this.jogadorLutador = data.jogadorLutador;
   }
-  
+
   preload() {
     this.load.spritesheet('jogadorAtirador', '../assets/GameScene/AtaqueDistancia.png', { frameWidth: 100, frameHeight: 100 });
     this.load.spritesheet('jogadorLutador', '../assets/GameScene/AtaqueCorpo.png', { frameWidth: 100, frameHeight: 100 });
@@ -30,33 +30,33 @@ class GameScene extends Phaser.Scene {
 
     this.larguraJogo = 1450;
     this.alturaJogo = 900;
-    
+    this.teclado = this.input.keyboard.createCursorKeys();
+    this.pontuacao = 0;
+    this.atirando = false;
+
     this.bg0 = this.add.tileSprite(this.cameras.main.centerX, this.cameras.main.centerY, 1450, 900, 'bg0').setScrollFactor(0).setDepth(-10);
     this.bg1 = this.add.tileSprite(this.cameras.main.centerX, this.cameras.main.centerY, 1450, 900, 'bg1').setScrollFactor(0).setDepth(-10);
     this.bg2 = this.add.tileSprite(this.cameras.main.centerX, this.cameras.main.centerY, 1450, 900, 'bg2').setScrollFactor(0).setDepth(-10);
     this.bg3 = this.add.tileSprite(this.cameras.main.centerX, this.cameras.main.centerY, 1450, 900, 'bg3').setScrollFactor(0).setDepth(-10);
     this.bg4 = this.add.tileSprite(this.cameras.main.centerX, this.cameras.main.centerY, 1450, 900, 'bg4').setScrollFactor(0).setDepth(-10);
     this.bg5 = this.add.tileSprite(this.cameras.main.centerX, this.cameras.main.centerY, 1450, 900, 'bg5').setScrollFactor(0).setDepth(-10);
-    
+
     // Definindo a flecha
     this.flecha = this.physics.add.image(0, 0, 'flecha').setScale(2);
     this.flecha.setVisible(false);
     this.flecha.body.setAllowGravity(false);
-    this.flecha.body.setSize(20,10);
-    
+    this.flecha.body.setSize(20, 10);
+
     // Definindo jogador
     this.jogador = this.physics.add.sprite(100, 850, 'jogadorParado').setScale(3).setDepth(1);
     this.jogador.body.setSize(20, 20);
-    
+
     // Adicione colisão entre o jogador e o chão
-    this.chao = this.physics.add.staticImage(0,900,'chao').setScale(3).refreshBody().setVisible(false);
+    this.chao = this.physics.add.staticImage(0, 900, 'chao').setScale(3).refreshBody().setVisible(false);
     this.physics.add.collider(this.jogador, this.chao);
-    
+
     this.cameras.main.startFollow(this.jogador);
     this.cameras.main.setBounds(0, 0, 3000, 600);
-
-    this.teclado = this.input.keyboard.createCursorKeys();
-    this.pontuacao = 0;
 
     this.anims.create({
       key: 'jogadorParadoA',
@@ -64,40 +64,40 @@ class GameScene extends Phaser.Scene {
       frameRate: 10,
       repeat: -1
     });
-    
+
     this.anims.create({
       key: 'jogadorAndandoA',
       frames: this.anims.generateFrameNumbers('jogadorAndando', { start: 0, end: 7 }),
       frameRate: 10,
       repeat: -1
     });
-    
+
     this.anims.create({
       key: 'jogadorMorteA',
       frames: this.anims.generateFrameNumbers('jogadorMorte', { start: 0, end: 3 }),
       frameRate: 10,
-      repeat: -1
+      repeat: 0
     });
-    
+
     this.anims.create({
       key: 'jogadorDanoA',
       frames: this.anims.generateFrameNumbers('jogadorDano', { start: 0, end: 3 }),
       frameRate: 10,
-      repeat: -1
+      repeat: 0
     });
 
     this.anims.create({
       key: 'jogadorAtiradorA',
       frames: this.anims.generateFrameNumbers('jogadorAtirador', { start: 0, end: 8 }),
       frameRate: 10,
-      repeat: -1
+      repeat: 0
     });
 
     this.anims.create({
       key: 'jogadorLutadorA',
       frames: this.anims.generateFrameNumbers('jogadorLutador', { start: 0, end: 5 }),
       frameRate: 10,
-      repeat: -1
+      repeat: 0
     });
 
     this.anims.create({
@@ -109,7 +109,8 @@ class GameScene extends Phaser.Scene {
 
   }
 
-  update(){
+  update() {
+    console.log(this.atirando);
     this.bg1.tilePositionX = this.cameras.main.scrollX * 0.3;
     this.bg2.tilePositionX = this.cameras.main.scrollX * 0.5;
     this.bg3.tilePositionX = this.cameras.main.scrollX * 0.7;
@@ -117,77 +118,83 @@ class GameScene extends Phaser.Scene {
     this.bg5.tilePositionX = this.cameras.main.scrollX;
     this.score = this.cameras.main.scrollX;
 
-    
 
     //função de movimentação
-    if(this.teclado.left.isDown){
+    if (this.teclado.left.isDown) {
       this.jogador.setVelocityX(-200);
       this.jogador.flipX = true;
-      this.jogador.anims.play('jogadorAndandoA', true);
     }
-    else if(this.teclado.right.isDown){
+    else if (this.teclado.right.isDown) {
       this.jogador.setVelocityX(200);
       this.jogador.flipX = false;
-      this.jogador.anims.play('jogadorAndandoA', true);
     }
-    else{
+    else if (!this.atirando){
       this.jogador.setVelocityX(0);
       this.jogador.anims.play('jogadorParadoA', true);
     }
+
+    if ((this.jogador.body.velocity.x >= 200 || this.jogador.body.velocity.x <= -200) && !this.atirando) {
+      this.jogador.anims.play('jogadorAndandoA', true);
+    }
+
     //função de pulo atirador
-    if(this.teclado.up.isDown && this.jogador.body.onFloor() && this.jogadorAtirador){
+    if (this.teclado.up.isDown && this.jogador.body.onFloor() && this.jogadorAtirador) {
       this.jogador.setVelocityY(-450);
     }
 
     //função de pulo lutador
-    if(this.teclado.up.isDown && this.jogador.body.onFloor() && this.jogadorLutador){
+    if (this.teclado.up.isDown && this.jogador.body.onFloor() && this.jogadorLutador) {
       this.jogador.setVelocityY(-250);
     }
 
-    //jogador Atirador
-    if(this.jogadorAtirador && this.teclado.space.isDown ){
-      this.jogador.anims.play('jogadorAtiradorA', true);
-    }
-
     //jogador Lutador 
-    if(this.jogadorLutador && this.teclado.space.isDown ){
+    if (this.jogadorLutador && this.teclado.space.isDown) {
       this.jogador.anims.play('jogadorLutadorA', true);
     }
-
-    if (teclado.space.isDown && !this.flecha.visible && teclado.left.isDown && this.jogadorAtirador) {
-      this.flecha.setPosition(this.jogador.x, this.jogador.y);
-      flechada();
-      this.flecha.flipX = true;
-      this.flecha.setVelocityX(-450);
-    } else if (teclado.space.isDown && !this.flecha.visible && teclado.right.isDown && this.jogadorAtirador) {
-      this.flecha.setPosition(this.jogador.x, this.jogador.y);
-      flechada();
-      this.flecha.flipX = true;
-      this.flecha.setVelocityX(450);
-    } else if (teclado.space.isDown && !this.flecha.visible && this.jogadorAtirador) {
-      this.flecha.setPosition(this.jogador.x, this.jogador.y);
-      flechada();
-      this.flecha.setVelocityY(-450);
+    // ataque flecha esquerdda 
+    if (this.teclado.space.isDown && !this.flecha.visible && this.teclado.left.isDown && this.jogadorAtirador) {
+      this.atirando = true;
+      this.jogador.anims.play('jogadorAtiradorA', true);
+      this.time.delayedCall(400, () => {
+        this.flecha.setPosition(this.jogador.x, this.jogador.y);
+        this.flechada();
+        this.flecha.flipX = true;
+        this.flecha.setVelocityX(-450);
+        this.atirando = false;
+      });
     }
-    
-    if(this.flecha.x < this.jogador.x - 200 || this.flecha.y > this.jogador.x + 200 || this.flecha.y < this.jogador.y - 200 ){
-      this.desativarFlechada;
+
+    //ataque flecha direita
+    if (this.teclado.space.isDown && !this.flecha.visible && this.teclado.right.isDown && this.jogadorAtirador) {
+      this.atirando = true;
+      this.jogador.anims.play('jogadorAtiradorA', true);
+      this.time.delayedCall(400, () => {
+        this.flecha.setPosition(this.jogador.x, this.jogador.y);
+        this.flechada();
+        this.flecha.flipX = false;
+        this.flecha.setVelocityX(450);
+        this.atirando = false;
+      });
+    }
+
+    if (this.flecha.x < this.jogador.x - 400 || this.flecha.x > this.jogador.x + 400 || this.flecha.y < this.jogador.y - 200) {
+      this.desativarFlechada();
     }
 
   }
 
-  flechada(){
+  flechada() {
     this.flecha.setVisible(true);
   }
 
-  desativarFlechada(){
+  desativarFlechada() {
     this.flecha.setVisible(false);
     this.flecha.setVelocityX(0);
     this.flecha.setVelocityY(0);
-    this.flecha.setPosition(0,0);
+    this.flecha.setPosition(0, 0);
   }
 
-  desativarAtaque(){
+  desativarAtaque() {
 
   }
 
